68ae7a02f50ea0deb7e45f12e13633946e4565ae
imyoubin <imyoubin@hanmail.net>
2025-05-18 22:43:58 +0900

    2025.05.18

--- src/com/javaex/ex05/EmployeeApp.java
+++ src/com/javaex/ex05/EmployeeApp.java
@@ -4,7 +4,7 @@ public class EmployeeApp {
 
     public static void main(String[] args) {
 
-        Employee pt = new Depart( "홍길동", 5000, "개발부" );
+    	Employee pt = new Depart( "홍길동", 5000, "개발부" );
         pt.showInformation();
     }
 
